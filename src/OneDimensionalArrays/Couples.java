ea84708a0e92d704aa332b9429027391e91d6d42
Yoav <yoav.meron11@gmail.com>
2021-10-27 15:03:57 +0300

    Finished couples assignment.

---
+++ src/OneDimensionalArrays/Couples.java
@@ -0,0 +1,36 @@
+package OneDimensionalArrays;
+import java.util.*;
+import java.util.Random;
+
+public class Couples {
+    public int[] array;
+    public int num;
+
+    Couples() {
+        Random random = new Random();
+        this.array = new int[random.nextInt(10)+5];
+        this.num = 15;
+
+        for (int i = 0; i < this.array.length; i++) {
+            this.array[i] = random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(this.array));
+    }
+    public void findCouples() {
+        for (int i = 0; i < this.array.length-1; i++) {
+            for (int j = i + 1; j < this.array.length; j++) {
+                int sum = this.array[i] + this.array[j];
+                int product = this.array[i] * this.array[j];
+                if (sum == this.num | product == this.num) {
+                    System.out.printf("(%d, %d)", this.array[i], this.array[j]);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Couples couples = new Couples();
+        couples.findCouples();
+    }
+}
