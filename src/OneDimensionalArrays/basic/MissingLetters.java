f805a1a9eb00784cbbd92e6b4b9ca6adfeb14aa0
Yoav <yoav.meron11@gmail.com>
2021-10-29 19:42:25 +0300

    Added file to basic dir

---
+++ src/OneDimensionalArrays/basic/MissingLetters.java
@@ -0,0 +1,43 @@
+package OneDimensionalArrays.basic;
+
+public class MissingLetters {
+    char[] alphabet = new char[52];
+    char[] array = new char[13];
+
+    MissingLetters() {
+        this.fillArrays();
+    }
+
+    public void fillArrays() {
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = (char) (65+i);
+            alphabet[alphabet.length-(i+1)] = (char) (97+i);
+        }
+        for (int i = 0; i < 13; i++) {
+            array[i] = alphabet[(int) (Math.random() * alphabet.length)];
+        }
+    }
+
+    public boolean checkIfCharInArray(char character) {
+        for (int element: array) {
+            if (element == character) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void checkAllLetters() {
+        System.out.println(this.array);
+        for (char element: alphabet) {
+            if (!this.checkIfCharInArray(element)) {
+                System.out.print(element + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MissingLetters missingLetters = new MissingLetters();
+        missingLetters.checkAllLetters();
+    }
+}
---
+++ src/OneDimensionalArrays/basic/Totals.java
@@ -0,0 +1,25 @@
+package OneDimensionalArrays.basic;
+
+public class Totals {
+    public static void main(String[] args) {
+        int[] array = {123, 54, 545, 254, 3, 5453, 1, 513};
+        int oddTotal = 0;
+        int evenTotal = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                oddTotal += array[i];
+            }
+            else {
+                evenTotal += array[i];
+            }
+        }
+        if (oddTotal == evenTotal) {
+            System.out.println("The total of the numbers in even indexes is equal.");
+
+        }
+        else {
+            System.out.println("The total of the numbers in even indexes is not equal.");
+        }
+        System.out.println(oddTotal - evenTotal);
+    }
+}
