aea3ff841c8900cd28e1005c7b4a37386e402372
Yoav <yoav.meron11@gmail.com>
2021-10-29 19:59:59 +0300

    Finished DollarChain task.

---
+++ src/OneDimensionalArrays/ArrayFunctions/DollarChain.java
@@ -0,0 +1,24 @@
+package OneDimensionalArrays.ArrayFunctions;
+
+public class DollarChain {
+    public boolean findDollarChain(char[] array, int len) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == '$') {
+                if ((i + len - 1) < array.length) {
+                    if (array[i + len - 1] == '$') {
+                        if (array[(i + (i + len)) / 2] == '$') {
+                            return true;
+                        }
+                }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DollarChain dollarChain = new DollarChain();
+        char[] array = {'$', '$', '$'};
+        System.out.println(dollarChain.findDollarChain(array, 3));
+    }
+}
