c97140ea2ceb88de6e1dc42138bab6e63d35aa19
Yoav <yoav.meron11@gmail.com>
2022-09-13 21:12:56 +0300

    Commit

--- src/ClassExercise/Line/Program.java
+++ src/ClassExercise/Line/Program.java
@@ -1,7 +1,9 @@
 package ClassExercise.Line;
 
+import java.util.Scanner;
+
 public class Program {
-    public Line longestLine(Line[] arr) {
+    public static Line longestLine(Line[] arr) {
         Line longestLine = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i].getLength() > longestLine.getLength()) {
@@ -11,7 +13,7 @@ public class Program {
         return longestLine;
     }
 
-    public int pointOnLines(Point point, Line[] lines) {
+    public static int pointOnLines(Point point, Line[] lines) {
         int counter = 0;
         for (int i = 0; i < lines.length; i++) {
             if (point.checkIfSamePoint(lines[i].getPointOne()) || point.checkIfSamePoint(lines[i].getPointTwo())) {
@@ -22,6 +24,32 @@ public class Program {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Line[] lineArray = new Line[10];
+        for (int i = 0; i < lineArray.length; i++) {
+            System.out.println("Enter the x coordinate for the first point >> ");
+            double pointOneX = scanner.nextDouble();
+            System.out.println("Enter the y coordinate for the first point >> ");
+            double pointOneY = scanner.nextDouble();
+            lineArray[i].setPointOne(new Point(pointOneX, pointOneY));
 
+            System.out.println("Enter the x coordinate for the second point >> ");
+            double pointTwoX = scanner.nextDouble();
+            System.out.println("Enter the y coordinate for the second point >> ");
+            double pointTwoY = scanner.nextDouble();
+            lineArray[i].setPointTwo(new Point(pointTwoX, pointTwoY));
+        }
+
+        System.out.println(Program.longestLine(lineArray));
+        System.out.println(pointOnLines(new Point(2,4), lineArray));
+
+        int counter = 0;
+        for (int i = 0; i < lineArray.length; i++) {
+            if (lineArray[i].parallelToY()) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter + " lines are parallel to the y axis.");
     }
 }
