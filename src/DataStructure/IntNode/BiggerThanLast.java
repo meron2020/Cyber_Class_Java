b5ecf5e301d2cdc147a53a92d42ea113cd978020
SamLizard <90714263+SamLizard@users.noreply.github.com>
2022-10-20 10:57:45 +0300

    commit

---
+++ src/DataStructure/IntNode/BiggerThanLast.java
@@ -0,0 +1,12 @@
+package DataStructure.IntNode;
+
+public class BiggerThanLast {
+    public static void biggerThanLast(IntNode firstInList) {
+        IntNode p = firstInList;
+        while (p.getNext() != null) {
+            if (p.getValue() < p.getNext().getValue()) {
+                System.out.println(p.getNext().getValue());
+            }
+        }
+    }
+}
