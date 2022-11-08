7e7cd2d29d31289a7070ec56ee8caa6dfd5c76fa
SamLizard <90714263+SamLizard@users.noreply.github.com>
2022-11-08 11:19:06 +0200

    commit

--- src/DataStructures/IntNode/ClassExercises/Page_76/DeleteNBiggestNumbers.java
+++ src/DataStructures/IntNode/ClassExercises/Page_76/DeleteNBiggestNumbers.java
@@ -1,22 +1,31 @@
 package DataStructures.IntNode.ClassExercises.Page_76;
 
 import DataStructures.IntNode.IntNode;
+
 //Page 76 Question 14
 public class DeleteNBiggestNumbers {
 
-    public static void deleteNumberFromList(IntNode list, int num) {
-
+    public static IntNode deleteNumberFromList(IntNode list, int num) {
+        while (list.getNext() != null) {
+            if (list.getNext().getValue() == num) {
+                list.setNext(list.getNext().getNext());
+                break;
+            }
+        }
+        return list;
     }
 
     public static void deleteNBiggestNumbers(IntNode list, int n) {
         IntNode p = new IntNode(0, list);
         int largestNum = p.getNext().getValue();
-        while (p.getNext() != null) {
-            if (p.getNext().getValue() > largestNum) {
-                largestNum = p.getNext().getValue();
+        for (int i = 0; i < n; i++) {
+            while (p.getNext() != null) {
+                if (p.getNext().getValue() > largestNum) {
+                    largestNum = p.getNext().getValue();
+                }
+                p = p.getNext();
             }
-            p = p.getNext();
+            list = deleteNumberFromList(list, largestNum);
         }
-
     }
 }
