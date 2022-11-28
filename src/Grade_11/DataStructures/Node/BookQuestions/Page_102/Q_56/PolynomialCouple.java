bf46893cec9e38d4c66447c77031353f189ff021
yoavm <yoav.meron11@gmail.com>
2022-11-28 17:02:19 +0200

    Commit

---
+++ src/Grade_11/DataStructures/Node/BookQuestions/Page_102/Q_56/PolynomialCouple.java
@@ -0,0 +1,30 @@
+package Grade_11.DataStructures.Node.BookQuestions.Page_102.Q_56;
+
+public class PolynomialCouple {
+    private int coefficient;
+
+    public PolynomialCouple(int coefficient, int degree) {
+        this.coefficient = coefficient;
+        this.degree = degree;
+    }
+
+    public int getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    private int degree;
+
+
+}
---
+++ src/Grade_11/DataStructures/Node/BookQuestions/Page_102/Q_56/Program.java
@@ -0,0 +1,51 @@
+package Grade_11.DataStructures.Node.BookQuestions.Page_102.Q_56;
+
+import Grade_11.DataStructures.Node.Node;
+
+public class Program {
+    public static Node<PolynomialCouple> addCoefficient(Node<PolynomialCouple> list,
+                                                        Node<PolynomialCouple> coupleToAdd) {
+
+        Node<PolynomialCouple> coupleToCheck = list;
+        while (coupleToCheck.getNext() != null) {
+            if (coupleToCheck.getValue().getDegree() == coupleToAdd.getValue().getDegree()) {
+                coupleToCheck.getValue().setCoefficient(coupleToCheck.getValue().getCoefficient() +
+                        coupleToAdd.getValue().getCoefficient());
+                return list;
+            }
+            else if (coupleToCheck.getValue().getDegree() > coupleToAdd.getValue().getDegree()) {
+                if (coupleToCheck.getNext().getValue().getDegree() < coupleToAdd.getValue().getDegree()) {
+                    coupleToAdd.setNext(coupleToCheck.getNext());
+                    coupleToCheck.setNext(coupleToAdd);
+                    return list;
+                }
+                coupleToCheck = coupleToCheck.getNext();
+            }
+            else {
+                coupleToAdd.setNext(coupleToCheck);
+                return coupleToAdd;
+            }
+        }
+        coupleToCheck.setNext(coupleToAdd);
+        return list;
+    }
+
+    public static Node<PolynomialCouple> polynomialAddition(Node<PolynomialCouple> list1, Node<PolynomialCouple> list2) {
+        Node<PolynomialCouple> listToReturn = new Node<PolynomialCouple>(new PolynomialCouple(1,1));
+        Node<PolynomialCouple> endOfList = listToReturn;
+        Node<PolynomialCouple> p = list1;
+        while (p != null) {
+            endOfList.setNext(p);
+            endOfList = endOfList.getNext();
+            p = p.getNext();
+        }
+        p = list2;
+        Node<PolynomialCouple> coupleToCheck = listToReturn;
+        while (p != null) {
+            listToReturn = addCoefficient(coupleToCheck, p);
+            p = p.getNext();
+        }
+
+        return listToReturn;
+    }
+}
