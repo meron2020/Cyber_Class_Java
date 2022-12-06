d4f03ce68fbc38b520cfe9f1e188f7db8ed86bd5
yoavmeron <yoav.meron11@gmail.com>
2022-12-06 11:33:17 +0200

    Exercise

--- src/Grade_11/DataStructures/Stack/P_Q121/Q7/BackToLastIP.java
+++ src/Grade_11/DataStructures/Stack/P_Q121/Q7/BackToLastIP.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 import Grade_11.DataStructures.Stack.P_Q121.Stack;
 
 public class BackToLastIP {
+    //My Solution
     public static String back(Stack<String> ips, String backType) {
         if (backType.equals("BackX")) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter how many addresses back you would like to move >> ");
             int backNum = scanner.nextInt();
-            if (backNum > 9 || backNum < 2) {
-                return "";
-            }
             int counter = 0;
             while (counter != backNum || !ips.isEmpty()) {
                 ips.pop();
@@ -32,5 +30,28 @@ public class BackToLastIP {
             return "";
         }
         return "";
+    }
+
+    //Class Solution
+    public static String goBack(Stack<String> s, String st) {
+        if (st.equals("Back")) {
+            if (s.isEmpty()) {
+                return "";
+            }
+            return s.pop();
+        }
+        else {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("How many times back?");
+            int x = reader.nextInt();
+            while (!s.isEmpty() && x > 1) {
+                s.pop();
+                x--;
+            }
+            if (s.isEmpty()) {
+                return "";
+            }
+            return s.pop();
+        }
     }
 }
