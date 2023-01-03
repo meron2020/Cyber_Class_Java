0f07413ed6c8b5bab13044ed590e830a06475960
SamLizard <90714263+SamLizard@users.noreply.github.com>
2023-01-03 11:40:54 +0200

    Commit

---
+++ src/Grade_11/Recursion/RecursiveExponent.java
@@ -0,0 +1,10 @@
+package Grade_11.Recursion;
+
+public class RecursiveExponent {
+    public static int recursiveExponent(int x, int y) {
+        if (y == 0) {
+            return 1;
+        }
+        return recursiveExponent(x, y - 1) * x;
+    }
+}
