70e98f68b71268a0f8e27f06f2e9803fc2771d38
SamLizard <90714263+SamLizard@users.noreply.github.com>
2023-01-10 12:19:41 +0200

    Commit

--- src/Grade_11/Recursion/ClassExercises/FindMaxInArr.java
+++ src/Grade_11/Recursion/ClassExercises/FindMaxInArr.java
@@ -6,6 +6,10 @@ public class FindMaxInArr {
     }
 
     public static int findMaxInArr(int[] arr, int i) {
+        if (i == 0) {
+            return arr[0];
+        }
 
+        return Math.max(arr[i], findMaxInArr(arr, i - 1));
     }
 }
