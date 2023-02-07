bffc32e951d65989dbcf711022ccd8d48a44b1e7
SamLizard <mr.fuchs10@gmail.com>
2023-02-07 11:27:24 +0200

    Commit

--- src/Grade_11/Recursion/ClassExercises/PrintNum.java
+++ src/Grade_11/Recursion/ClassExercises/PrintNum.java
@@ -2,13 +2,13 @@ package Grade_11.Recursion.ClassExercises;
 
 public class PrintNum {
     public static void printDigit(int num) {
-        if (num > 9) {
+        if (num > 0) {
             printDigit(num / 10);
+            System.out.println(num % 10);
         }
-        System.out.println(num % 10);
     }
 
     public static void main(String[] args) {
-        printDigit(5412);
+        printDigit(5490);
     }
 }
