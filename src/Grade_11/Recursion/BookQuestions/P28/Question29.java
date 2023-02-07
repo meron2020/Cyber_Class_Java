3ffc31d23c6e844ea26adc91d3e7b834a5a32853
SamLizard <mr.fuchs10@gmail.com>
2023-02-07 13:31:00 +0200

    Commit

--- src/Grade_11/Recursion/BookQuestions/P28/Question29.java
+++ src/Grade_11/Recursion/BookQuestions/P28/Question29.java
@@ -1,4 +1,14 @@
 package Grade_11.Recursion.BookQuestions.P28;
 
 public class Question29 {
+    public static void numsByDifference(int num, int d, int n) {
+        if (n > 0) {
+            System.out.println(num);
+            numsByDifference(num + d, d + 1, n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        numsByDifference(1, 1, 6);
+    }
 }
---
+++ src/Grade_11/Recursion/BookQuestions/P28/Question30.java
@@ -0,0 +1,7 @@
+package Grade_11.Recursion.BookQuestions.P28;
+
+public class Question30 {
+    public static void series(int n, int num) {
+
+    }
+}
