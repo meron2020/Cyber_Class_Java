0b5281c62aa848c808545f0803c5cd8773fd22e9
SamLizard <mr.fuchs10@gmail.com>
2023-02-28 11:33:15 +0200

    Commit

--- src/Grade_11/DataStructures/BinNode/Page176/Question11.java
+++ src/Grade_11/DataStructures/BinNode/Page176/Question11.java
@@ -3,17 +3,37 @@ package Grade_11.DataStructures.BinNode.Page176;
 import Grade_11.DataStructures.BinNode.BinNode;
 
 public class Question11 {
+    // My solution
     public static void printAllEven(BinNode<Integer> t) {
         if (t != null) {
-           printAllEven(t.getLeft());
-           printAllEven(t.getRight());
-        }
-        if (t.getValue() % 2 == 0) {
-            if (!t.hasRight() && !t.hasLeft()) {
-                System.out.println(t.getValue());
+            if (t.getValue() % 2 == 0) {
+                if (!t.hasRight() && !t.hasLeft()) {
+                    System.out.println(t.getValue());
+                } else if (!(t.getRight().getValue() % 2 == 1 || t.getLeft().getValue() % 2 == 1)) {
+                    System.out.println(t.getValue());
+                }
             }
-            else if (!(t.getRight().getValue() % 2 == 1 || t.getLeft().getValue() % 2 == 1)) {
-                System.out.println(t.getValue());
+            printAllEven(t.getLeft());
+            printAllEven(t.getRight());
+        }
+
+    }
+
+    public static void printEven(BinNode<Integer> t) {
+        if (t != null) {
+            if (t.getValue() % 2 == 0) {
+                boolean ok = true;
+                if (t.hasLeft() && t.getLeft().getValue() % 2 == 0) {
+                    ok = false;
+                }
+                if (t.hasRight() && t.getRight().getValue() % 2 == 0) {
+                    ok = false;
+                }
+                if (ok) {
+                    System.out.println(t.getValue());
+                }
+                printEven(t.getLeft());
+                printEven(t.getRight());
             }
         }
     }
